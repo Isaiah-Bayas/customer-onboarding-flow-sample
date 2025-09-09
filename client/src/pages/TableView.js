b9db25e76da7122332c4b05d387440150018e062
Isaiah-Bayas <33269742+Isaiah-Bayas@users.noreply.github.com>
2025-09-09 21:04:59 +0000

    deployment ready

--- client/src/pages/TableView.js
+++ client/src/pages/TableView.js
@@ -1,16 +1,22 @@
 // client/src/pages/TableView.js
 import React, { useEffect, useState } from "react";
 import Table from "../components/Table";
+import axios from "axios";
+
+const API = (process.env.REACT_APP_API_BASE_URL || "http://localhost:4000").replace(/\/$/, "");
 
 function TableView() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("/data")
-      .then((res) => res.json())
-      .then((json) => {
-        setData(json);
+    axios.get(`${API}/data`)
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
         setLoading(false);
       });
   }, []);
